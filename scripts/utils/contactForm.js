54eee197f5be5e5c4c08ca9478d5e02373bad670
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2022-05-02 19:02:32 +0200

    ajout_price + modification_modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,7 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    body.style.background = "green";
 }
 
 function closeModal() {
