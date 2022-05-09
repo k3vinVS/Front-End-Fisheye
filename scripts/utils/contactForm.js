bfac6eff0635553c945220811b664e7e52f3ead0
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2022-05-09 15:00:54 +0200

    ajout form-javascript_photographer.html

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,28 +1,45 @@
 // DOM ELEMENTS
 const form = document.querySelector('form');
-const inputFirstName = document.querySelector('#first');
-const inputLastName = document.querySelector('#last');
-const inputEmail = document.querySelector('#email');
+const inputFirstName = document.getElementById('first');
+const inputLastName = document.getElementById('last');
+const inputEmail = document.getElementById('email');
+const inputMessage = document.getElementById('message');
+
 
 
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+	const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+	const modal = document.getElementById("contact_modal");
+	modal.style.display = "none";
+	window.location.reload();
 }
 
 
+// Valide d'une couleur verte les inputs du formulaire
+function validInput(input) {
+	input.style.border = 'none';
+}
+
+
+// Invalide d'une couleur rouge les inputs du formulaire
+function invalidInput(input) {
+	input.style.border = '2px solid red';
+}
+
+
+
 // Ecoute de l'input du prénom
-inputFirstName.addEventListener('input', function() {
+inputFirstName.addEventListener('input', function () {
 	validFirstName(this);
 });
 
 // Vérifie que l'input est renseigné, et le regex permet d'autoriser certains caractères, avec un minimum de 2 lettres (mets de côté les chiffres)
-const validFirstName = function(acceptFirstName) {
+const validFirstName = function (acceptFirstName) {
+
 	let small = inputFirstName.nextElementSibling;
 	let firstNameRegExp = new RegExp(
 		'^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð-]{2,}$',
@@ -42,12 +59,12 @@ const validFirstName = function(acceptFirstName) {
 
 
 // Ecoute de l'input du nom
-inputLastName.addEventListener('input', function() {
+inputLastName.addEventListener('input', function () {
 	validLastName(this);
 });
 
 // Vérifie que l'input est renseigné, et le regex permet d'autoriser certains caractères, avec un minimum de 2 lettres (mets de côté les chiffres)
-const validLastName = function(acceptLastName) {
+const validLastName = function (acceptLastName) {
 	let small = inputLastName.nextElementSibling;
 	let lastNameRegExp = new RegExp(
 		'^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð-]{2,}$',
@@ -67,26 +84,60 @@ const validLastName = function(acceptLastName) {
 
 
 // Ecoute de l'input de l'adresse email
-inputEmail.addEventListener('change', function() {
+inputEmail.addEventListener('change', function () {
 	validEmail(this);
 });
 
 // Vérifie que l'input est renseigné, et le regex permet d'autoriser un certain nombre de caractères et l'obligation de symboles (comme toutes les adresses emails)
-const validEmail = function(acceptEmail) {
+const validEmail = function (acceptEmail) {
 	let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
 	let small = inputEmail.nextElementSibling;
 
 	if (emailRegExp.test(acceptEmail.value)) {
-		// validInput(inputEmail);
+		validInput(inputEmail);
 		small.style.color = 'green';
-        alert('vrai');
-		// small.innerHTML = 'Adresse email valide';
+		small.innerHTML = 'Adresse email valide';
 		return true;
 	} else {
-		// invalidInput(inputEmail);
+		invalidInput(inputEmail);
 		small.style.color = 'red';
-        alert('faux');
-		// small.innerHTML = 'Adresse email non valide';
+		small.innerHTML = 'Adresse email non valide';
 		return false;
 	}
 };
+
+
+inputMessage.addEventListener('input', function () {
+	validMessage(this);
+});
+
+const validMessage = function (acceptMessage) {
+	let small = inputMessage.nextElementSibling;
+
+	if (acceptMessage.value) {
+		validInput(inputMessage);
+		small.innerHTML = '';
+		return true;
+	} else {
+		invalidInput(inputMessage);
+		small.innerHTML = 'Veuillez renseigner un message';
+		return false;
+	}
+}
+
+form.addEventListener('submit', function () {
+
+	// Vérifie que les inputs sont remplis et valide
+	if (
+		validFirstName(inputFirstName) &&
+		validLastName(inputLastName) &&
+		validEmail(inputEmail) &&
+		validMessage(inputMessage)
+	) {
+		return true;
+		
+	} else {	
+		return false;
+	}
+});
+
